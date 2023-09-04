1c03cd5cbd32cf87b3167e71f8edf311376cb49d
soja <solene.jabaud@gmail.com>
2023-09-04 11:41:22 +0200

    test on axios

--- src/services/axios-config.js
+++ src/services/axios-config.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
-import Cookies from 'js-cookie';
+// import axios from 'axios';
+// import Cookies from 'js-cookie';
 
-const API = axios.create({
-  baseURL: 'http://127.0.0.1:3000'
-});
+// const API = axios.create({
+//   baseURL: 'http://127.0.0.1:3000'
+// });
 
-API.interceptors.request.use(({ headers, ...config }) => ({
-  ...config,
-  headers: {
-    ...headers,
-    'Content-Type': 'application/json',
-    Authorization: `Bearer ${headers.Authorization ? headers.Authorization : Cookies.get('token')}`,
-  },
-}));
+// API.interceptors.request.use(({ headers, ...config }) => ({
+//   ...config,
+//   headers: {
+//     ...headers,
+//     'Content-Type': 'application/json',
+//     Authorization: `Bearer ${headers.Authorization ? headers.Authorization : Cookies.get('token')}`,
+//   },
+// }));
 
-export default API;
+// export default API;
