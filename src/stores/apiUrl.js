5e257b1a752df59077159a5978112cfaf8740ef6
soja <solene.jabaud@gmail.com>
2023-09-04 11:16:14 +0200

    test deploy prod

--- src/stores/apiUrl.js
+++ src/stores/apiUrl.js
@@ -1,2 +1,2 @@
 // export const API_URL = "http://127.0.0.1:3000";
-export const API_URL = "https://marketplaceback-dbd5aad55b7d.herokuapp.com"
+export const API_URL = "https://immotep-2b5176fcfc83.herokuapp.com/"
