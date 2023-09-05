17d539580fef4da91049aca966c75201dd406acd
soja <solene.jabaud@gmail.com>
2023-09-05 12:24:45 +0200

    fix search by city

--- src/components/Properties/index.jsx
+++ src/components/Properties/index.jsx
@@ -24,6 +24,7 @@ const Properties = () => {
         const jsonData = await response.json();
         const reversedData = jsonData.reverse();
         setProperties(reversedData);
+        setSelectedProperties(reversedData);
       } else {
         throw new Error('Erreur lors de la requête');
       }
@@ -36,44 +37,54 @@ const Properties = () => {
 
 const searchByCity = (e) => {
   e.preventDefault();
-  setSelectedProperties(properties)
 
   if (city !== "") {
     setSelectedProperties(properties.filter(element => element.city && element.city.toLowerCase().includes(city.toLowerCase())))
-  } 
+  } else {
+    setSelectedProperties(properties)
+  }
 }
 
 
-return (
-  <div>
-    <h1> Trier par ville </h1>
+  return (
     <div>
-    <form onSubmit={searchByCity}>
-      <input
-      type="text"
-      placeholder="Ville"
-      value={city}
-      onChange={(e) => setCity(e.target.value)}/>
-      <button type="submit">Rechercher</button>
-    </form>
+      <h1> Trier par ville </h1>
+      <div>
+        <form onSubmit={searchByCity}>
+          <input
+            type="text"
+            placeholder="Ville"
+            value={city}
+            onChange={(e) => setCity(e.target.value)}
+          />
+          <button type="submit">Rechercher</button>
+        </form>
+      </div>
+      <div>
+        {selectedProperties.length > 0 ? (
+          <>
+            <h3>Voici la liste des biens</h3>
+            {selectedProperties.map((property) => {
+              return (
+                <div key={property.id}>
+                  <p>annonce n° : {property.id}</p>
+                  <p>titre : {property.title} </p>
+                  <p>description : {property.description}</p>
+                  <p>prix : {property.price}</p>
+                  <p> ville : {property.city} </p>
+                  <p><img src={property.image} alt={property.title} /> </p>
+                  <Link to={`/property/${property.id}`}>en savoir plus</Link>
+                  <p>*******************</p>
+                </div>
+              );
+            })}
+          </>
+        ) : (
+          <p>aucune propriété dans cette ville</p>
+        )}
+      </div>
     </div>
-    <h3>Voici la liste des biens</h3>
-      {selectedProperties.map(property => {
-        return (
-          <div key={property.id}>
-            <p>annonce n° : {property.id}</p>
-            <p>titre : {property.title} </p>
-            <p>description : {property.description}</p>
-            <p>prix : {property.price}</p>
-            <p> ville : {property.city} </p>
-            <p><img src={property.image} alt={property.title} /> </p>
-            <Link to={`/property/${property.id}`}>en savoir plus</Link>
-            <p>*******************</p>
-          </div>
-        )
-      })}
-  </div>
-)
+  );
 }
 
 export default Properties
